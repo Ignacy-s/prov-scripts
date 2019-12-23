40b9c2ef549aeb0eec7863458769c9cc8db54eae
Ignacy Sawa <bd1cb5c6@opayq.com>
2019-12-23 20:28:01 +0100

    Made update ssh config work when run for the first time v.2.

--- update_ssh_config.sh
+++ update_ssh_config.sh
@@ -33,7 +33,6 @@ fi
 start_line="#Vagrant Projects START"
 end_line="#Vagrant Projects END"
     
-
 # Check if there is at most one of START and STOP lines.
 for line in "$start_line" "$end_line"; do
     how_many_lines="$(grep -c "$line" ~/.ssh/config)"
@@ -44,26 +43,17 @@ for line in "$start_line" "$end_line"; do
     fi
 done
 
-
 # Check if script is run for the first time
 # (by checking if $start_line exists in the file)
 if [ "$(grep -c "$start_line" ~/.ssh/config)" -eq 0 ]; then
     echo "Welcome aboard!"
-    where_it_started=$(sed -n "$ =" ~/.ssh/config)
 else
-    # Remember where_it_started (the Vagrant block in ~/.ssh/config)
+# Remember where_it_started (the Vagrant block in ~/.ssh/config)
     where_it_started=$(sed -n  "/$start_line/ =" ~/.ssh/config)
     if [ $debug_mode -eq 1 ]; then
-        echo "Block starts at: $where_it_started"
+	echo "Block starts at: $where_it_started"
     fi
 fi
-
-# Check if script is run for the first time
-# (by checking if $start_line exists in the file)
-if [ "$(grep -c "$start_line" ~/.ssh/config)" -eq 0 ]; then
-    echo "Welcome aboard!"
-
-
 # Remove old entries.
 sed -i "/$start_line/,/$end_line/ d" ~/.ssh/config 
 
