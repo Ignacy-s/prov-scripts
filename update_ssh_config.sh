1031a27219ac9a8c0c41b99a58c58f458cf7278d
Ignacy Sawa <bd1cb5c6@opayq.com>
2019-12-23 15:30:59 +0100

    Tidied up the s cript, turning off dbg mode.

--- update_ssh_config.sh
+++ update_ssh_config.sh
@@ -1,23 +1,15 @@
 #! /bin/bash
 
-# For Vagrant and LIRW course
-
-# Removing old and adding new .ssh/config entries for servers that are
-# listed in servers_list file.  Expect FUN if server in server's list
-# has same name as some other server in ssh config.
-# 
-# Wed 18 Dec 2019 06:23:42 PM CET Ignacy
-# Attempting to change the script to stop using servers_list and
-# instead take a list of running servers from vagrant global status.
-
+# Add running Vagrant servers to ~/.ssh/config, so they can be
+# accessed with ssh $server_name or from inside emacs.
 
 #Debugging variables
-set -e
-set -x
-debug_mode=1
+#set -e
+#set -x
+debug_mode=0
 
 
-# Backup, but only once per hour
+# Backup ~/.ssh/config, but only once per hour
 if [[ ! -e ~/.ssh/config.bak.$(( $(date +%s)/(60*60) )) ]]; then
     if [ $debug_mode -eq 1 ]; then
 	echo "backing up config to config."$(($(date +%s)/(60*60)))
@@ -27,16 +19,6 @@ elif [ $debug_mode -eq 1 ]; then
     echo ".ssh/confing already backed up this hour."
 fi
 
-
-# Adding new entries can be done two ways. Since now I can use sed 'i'
-# function, I could just insert each server config batch in the right
-# place. Instead decided to upgrade the old method. The for loop can
-# be inside the curly braces, so that the block init and close lines
-# can be written only ones. Need to trace the pointer somehow. Started
-# playing with where it ends variable, but then remembered, that I'm
-# still reading the unchanged ssh config and writing into a temp ssh
-# config.
-
 # Create an array with names of running servers.
 unset servers_array
 for i in $(vagrant global-status --prune | grep running | \
@@ -44,12 +26,13 @@ for i in $(vagrant global-status --prune | grep running | \
     do servers_array+=( "${i##*/}" );
 done
 if [ $debug_mode -eq 1 ]; then
-    echo "Oto lista serwerow: ${servers_array[*]}"
+    echo "Servers list: ${servers_array[*]}"
 fi
 
 # Define start and end line strings.
 start_line="#Vagrant Projects START"
 end_line="#Vagrant Projects END"
+
 # Check if there is only one of START and STOP lines.
 for line in "$start_line" "$end_line"; do
     how_many_lines="$(grep -c "$line" ~/.ssh/config)"
@@ -60,20 +43,18 @@ for line in "$start_line" "$end_line"; do
     fi
 done
 
-# Remember where_it_started (the Vagrant block in ssh_config)
+# Remember where_it_started (the Vagrant block in ~/.ssh/config)
 where_it_started=$(sed -n  "/$start_line/ =" ~/.ssh/config)
 if [ $debug_mode -eq 1 ]; then
     echo "Block starts at: $where_it_started"
 fi
-#travelling exit 0 to debug the script step by step.
-exit 0
 
 # Remove old entries.
 sed -i "/$start_line/,/$end_line/ d" ~/.ssh/config 
 
 # Add new entries to .ssh/config
 {
-    #this will "save" the content of ssh_config
+    #this will "save" the content of ~/.ssh/config
 head -$((where_it_started - 1)) ~/.ssh/config
 echo "$start_line"
 for sys in "${servers_array[@]}"
@@ -86,5 +67,6 @@ do
 done
 echo "$end_line"
 tail -n +$where_it_started ~/.ssh/config; } > ~/.ssh/config-tmp
+
 mv ~/.ssh/config-tmp ~/.ssh/config
 exit 0
