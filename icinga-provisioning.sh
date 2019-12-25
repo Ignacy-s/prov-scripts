ea1d7e6f67277aff0512ec659775509b8f2ea736
Ignacy Sawa <bd1cb5c6@opayq.com>
2019-12-25 18:45:49 +0100

    Fixed small errors and indentation.

--- icinga-provisioning.sh
+++ icinga-provisioning.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-if [ ! \( "$(whoami)" = "root" \) ]
-then {
-echo "This should be run as root."
-exit 1
-}
+if [ ! \( "$(whoami)" = "root" \) ] ; then
+  echo "This should be run as root."
+  exit 1
 fi
 
 # Using set -e for debugging. It might crash scripts using subshells.
 set -e
+set -x
+
 yum update -y || true
 
 # Install the EPEL repo:
@@ -66,40 +66,36 @@ yum install rh-php71-php-mysqlnd
 mystring="date.timezone = \"Europe/Oslo\""
 php_path1="/etc/php.ini"
 php_path2="/etc/opt/rh/rh-php71/php.ini"
-for php_path in php_path1 php_path2;
-do
-if [ "$(grep -c "$mystring"  "$php_path")" -lt 1 ]
-then {
-cp -v "$php_path" ${php_path}.bak-$(($(date +%s)/60))
-echo "date.timezone = \"Europe/Oslo\"" | tee -a "$php_path"
-}
-else echo "Timezone already set."
-fi
+for php_path in $php_path1 $php_path2; do
+    if [ "$(grep -c "$mystring"  "$php_path")" -lt 1 ]; then
+	cp -v "$php_path" ${php_path}.bak-$(($(date +%s)/60))
+	echo "date.timezone = \"Europe/Oslo\"" | tee -a "$php_path"
+    else echo "Timezone already set."
+    fi
+done
+
 # Restart php fpm
 systemctl restart rh-php71-php-fpm.service
 
 #Configure the Database
-if [ "$(mysqlshow -u root -proot123 icinga | wc -l)" -eq 5 ]
-then
-mysql -v -u root -proot123 icinga < /usr/share/icinga2-ido-mysql/schema/mysql.sql
-elif [ "$(mysqlshow -u root -proot123 icinga | wc -l)" -eq 66 ]
-then {
-echo "Database already initialized"
-sleep 1;}
-else {
-echo "Database length neither 5 or 66, something's wrong?"
-exit 1
-}
+if [ "$(mysqlshow -u root -proot123 icinga | wc -l)" -eq 5 ]; then
+    mysql -v -u root -proot123 icinga < \
+	  /usr/share/icinga2-ido-mysql/schema/mysql.sql
+elif [ "$(mysqlshow -u root -proot123 icinga | wc -l)" -eq 66 ]; then
+    echo "Database already initialized"
+else 
+    echo "Database length neither 5 or 66, something's wrong?"
+    exit 1
 fi
 
 # Configure postfix
 mystring="smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd"
-if [ "$(grep -c "$mystring" /etc/postfix/main.cf)" -lt 1 ]
-then {
-yum install cyrus-sasl-plain -y #SASL auth lib
-cp -v /etc/postfix/main.cf /etc/postfix/main.cf.bak-$(($(date +%s)/60))
-tee -a /etc/postfix/main.cf <<EOF
-"smtp_sasl_auth_enable = yes
+if [ "$(grep -c "$mystring" /etc/postfix/main.cf)" -lt 1 ]; then
+    yum install cyrus-sasl-plain -y #SASL auth lib
+    cp -v /etc/postfix/main.cf \
+       /etc/postfix/main.cf.bak-$(($(date +%s)/60))
+    tee -a /etc/postfix/main.cf <<EOF
+    "smtp_sasl_auth_enable = yes
 smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
 smtp_sasl_security_options = noanonymous
 smtp_sasl_tls_security_options = noanonymous
@@ -107,29 +103,25 @@ smtp_tls_security_level = encrypt
 header_size_limit = 4096000
 relayhost = [smtp.sendgrid.net]:587"
 EOF
-cp /vagrant/sasl_passwd /etc/postfix/sasl_passwd
-chmod -v 600 /etc/postfix/sasl_passwd
-postmap -v /etc/postfix/sasl_passwd
-systemctl restart postfix.service
-rm -vf /etc/postfix/sasl_passwd
-}
+    cp /vagrant/sasl_passwd /etc/postfix/sasl_passwd
+    chmod -v 600 /etc/postfix/sasl_passwd
+    postmap -v /etc/postfix/sasl_passwd
+    systemctl restart postfix.service
+    rm -vf /etc/postfix/sasl_passwd
 else echo "Postfix already configured."
 fi
 
 # Tell icinga how to connect to the database
 mystring="password = \"icinga123\""
 my_url="/etc/icinga2/features-available/ido-mysql.conf"
-if [ "$(grep -c "$mystring" "$my_url" )" -lt 1 ]
-then {
+if [ "$(grep -c "$mystring" "$my_url" )" -lt 1 ]; then
     sed -E -i.bak-$(($(date +%s)/60)) -e '/\/\/(us|pa|ho|da)/s/\/\///' \
--e '/password/s/inga/inga123/' $my_url
-   
-}
+	-e '/password/s/inga/inga123/' $my_url
 else echo "DB password in icinga already set."
 fi
+
 # Enable ido-mysql.conf
 icinga2 feature enable ido-mysql
-
 
 # Allow commands to be received by the Web Front End
 icinga2 feature enable command
@@ -145,27 +137,22 @@ icinga_ver="$(icinga2 --version | head -1 | awk '{print $10}')"
 icinga_ver="${icinga_ver/#v/}"
 icinga_ver="${icinga_ver%.*}"
 echo "Found out that icinga version is: ${icinga_ver}."
-if [ "$icinga_ver" = "2.5" ]
-   then {
-#case when we have icinga 2.5 (from jason's tutorial)
-   if [ ! \( -e '/etc/icinga2/conf.d/api-users.conf' \) ]
-then {
-icinga2 node wizard <<EOF
+if [ "$icinga_ver" = "2.5" ];then
+    #case when we have icinga 2.5 (from jason's tutorial)
+    if [ ! \( -e '/etc/icinga2/conf.d/api-users.conf' \) ]; then
+	icinga2 node wizard <<EOF
 n
 
 
 
 EOF
-##That is we answer n, and 3 times enter to accept defaults
-} else echo "Icinga Config-Wizard already run."
-fi
-}
-elif [ $icinga_ver = "2.11" ] ; then 
-{
-#case when we have icinga 2.11 (had to find it out myself)
-   if [ ! \( -e '/etc/icinga2/conf.d/api-users.conf' \) ]
-then {
-icinga2 node wizard <<EOF
+	##That is we answer n, and 3 times enter to accept defaults
+    else echo "Icinga Config-Wizard already run."
+    fi
+elif [ "$icinga_ver" = "2.11" ] ; then
+    #case when we have icinga 2.11 (had to find it out myself)
+    if [ ! \( -e '/etc/icinga2/conf.d/api-users.conf' \) ];then
+	icinga2 node wizard <<EOF
 n
 
 
@@ -175,9 +162,8 @@ n
 
 EOF
 ##That is we answer n, and 6 times enter to accept defaults
-} else echo "Icinga Config-Wizard already run."
-fi
-}
+    else echo "Icinga Config-Wizard already run."
+    fi
 else echo "Unknown Icinga version: ${icinga_ver}."
      exit 1
 fi
